462c972799563ca7421d7f6ab2b22605ece28222
TinyPin146 <tinypin14@gmail.com>
2022-01-04 17:16:53 +0100

    Started digit and operator button handling functions

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const digitButtons = document.querySelectorAll('.digit-button button');
+digitButtons.forEach(button => button.addEventListener('click', handleDigitButtons))
+
 const operatorButtons = document.querySelectorAll('.operator-button button');
+operatorButtons.forEach(button => button.addEventListener('click', handleOperatorButtons))
+
 const digitScreen = document.querySelector('.digit-screen p')
+
+let arrOfDigits = [];
+let currentNum = null; 
+let arrOfNumbers = [];
 
 const add = function() {
     return arguments[0] + arguments[1];
@@ -19,3 +27,21 @@ const multiply = function() {
     }
     return mltplyResult;
   };
+
+  function handleDigitButtons(e) {
+    arrOfDigits.push(Number(e.currentTarget.textContent));
+
+    currentNum = Number(arrOfDigits.join(''));
+    digitScreen.textContent = currentNum;
+  }
+
+  function handleOperatorButtons(e) {
+    console.log(e.currentTarget.textContent);
+    switch (e.currentTarget.textContent) {
+      case '+': 
+        console.log('its a +')
+        break
+      default:
+        console.log('How did you fuck this up?');
+    }
+  }
